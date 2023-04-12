46bfe616328c6c5c7b4dcd606fc4594ea0cf5f04
wxy <xywang1106@163.com>
2023-04-12 11:01:23 +0800

    update conifg_next.js

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -20,8 +20,8 @@ const CONFIG_NEXT = {
 
   // 菜单
   MENU_HOME: true, // 显示首页
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
